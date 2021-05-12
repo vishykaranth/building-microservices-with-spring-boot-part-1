32dc939d8fc073cb11fc23b0bdbba6cab7a29861
vishykaranth <vishykaranth@gmail.com>
2021-05-12 12:27:19 +0530

    Microservices with Zuul Gateway

---
+++ customer-service/src/main/java/com/github/jrhenderson1988/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.jrhenderson1988.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CustomerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+}
